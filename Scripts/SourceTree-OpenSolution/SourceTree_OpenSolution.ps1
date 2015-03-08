b3b007a1373f3cf04d1030f34e4c00dd77a13ca2
AshleyPoole <git@ashleypoole.co.uk>
2015-03-08 20:02:00 +0000

    Adding SourceTree_OpenSolution.ps1

---
+++ Scripts/SourceTree-OpenSolution/SourceTree_OpenSolution.ps1
@@ -0,0 +1,13 @@
+﻿########################################################################
+# Created By Ashley Poole - http://www.ashleypoole.co.uk               #
+# Opens solution file(s) within a specified repository                 #
+# SourceTree_OpenSolutions.ps1                                         #
+########################################################################
+
+Param(
+    [string]
+    $Repo
+)
+
+# Get and open solution files
+Get-ChildItem $Repo -Recurse -Filter '*.sln' | Select { Start $_.FullName }
